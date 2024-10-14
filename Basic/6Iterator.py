b7a281ec39c4a2d67fdc58f4b5fb95b3b4231865
Moc <moc_11355@163.com>
2024-10-14 20:35:15 +0800

    create iterator doc

--- Basic/6Iterator.py
+++ Basic/6Iterator.py
@@ -35,14 +35,63 @@ dic = dic.__iter__()
 while 1:
     try:
         print(dic.__next__())
-    except StopIteration: # 当发生 这个错误的时候就会执行 这个代码块
+    except StopIteration:  # 当发生 这个错误的时候就会执行 这个代码块
 
         break
 
 # 迭代器调用 __iter__ 方法得到的结果是本身   | 统一 for循环的运行机制
 for i in dic:
     print(i)
+
+
 # 运行逻辑:
 # 1 不管对象是迭代器还是可迭代对象,都调用__iter__ 方法 获取迭代器版本
 # 2 调用迭代器的 __next__ 方法,拿到值反馈给 i
 # 3 循环执行第二步 直到出现异常,结束循环
+
+
+# 生成器
+def ff():
+    print(11111)
+    yield 1
+    print(22222)
+    yield 2
+    print(333333)
+    yield 3
+
+
+res = ff()  #
+print(id(res))  # yield 返回的是一个生成器  -- 生成器 其实就是迭代器
+res.__next__()  # 函数会执行到 第一个 yield 返回结果后停止
+res.__next__()  # 函数继续执行 到第二个yield 然后停止
+l = [1, 2, 1, 3, 1, 4, 1, 5]
+# 平时统计长度
+len(l)
+# 等同于
+l.__len__()
+
+# 所以 调用 __next__()  可以这样写
+next(res)
+# __iter__() 也可以 写为
+iter(res)
+
+
+# yield 表达式
+
+def func(x):
+    print(f'{11} kaishi ')
+    n = 0
+    while " ":
+        # 将yield 赋值给变量之后 就可以尝试 接收值
+        y = yield n
+        print(x, '----------', y)
+        n += 1
+
+
+g = func(20)
+g.send(None)  # 刚创建的生成器 是不可以接收值的
+g.send(100)  # 此时 g.send 的值就会传递给 yield 然后赋值给 y
+
+for i in func(10):
+    l = [1, 2, 3]
+    i.send(l)
